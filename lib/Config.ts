7e88cf05fa288858f961ec2ca4060878f2e99642
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-08-29 12:34:13 +0700

    Update length

--- lib/Config.ts
+++ lib/Config.ts
@@ -290,14 +290,14 @@ export class Config implements IConfig {
     /**
      * The length of a standard address for your coin
      */
-    public standardAddressLength: number = 99;
+    public standardAddressLength: number = 98;
 
     /* The length of an integrated address for your coin - It's the same as
        a normal address, but there is a paymentID included in there - since
        payment ID's are 64 chars, and base58 encoding is done by encoding
        chunks of 8 chars at once into blocks of 11 chars, we can calculate
        this automatically */
-    public integratedAddressLength: number = 99 + ((64 * 11) / 8);
+    public integratedAddressLength: number = 98 + ((64 * 11) / 8);
 
     /**
      * A replacement function for the JS/C++ underivePublicKey.
