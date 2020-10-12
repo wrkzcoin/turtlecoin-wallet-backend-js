462ed75952e5b437a6a243cf87731200f366f4e6
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2020-10-12 02:48:46 +0100

    Typo

--- lib/WalletBackend.ts
+++ lib/WalletBackend.ts
@@ -1457,7 +1457,7 @@ export class WalletBackend extends EventEmitter {
         }
 
         this.config.scanCoinbaseTransactions = shouldScan;
-        this.daemon.updateConfig(this.config.scanCoinbaseTransactions);
+        this.daemon.updateConfig(this.config);
     }
 
     /**
